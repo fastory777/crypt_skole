3716605609365e888176fe31b2b1d0f6b4047403
Arseniy Domanov <decapitator0702@gmail.com>
2026-04-08 11:24:05 +0200

    Server and index update, gitignore update

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+require("./private/db.pri.js");
+
 const { block } = require("./private/block.pri");
+const { run: dbRun } = require('./private/db.pri');
 
 app.post("/api/block", (req, res) => {
     const verdi = req.body.value;
@@ -13,6 +16,15 @@ app.post("/api/block", (req, res) => {
     const blokk = block(verdi);
     res.json({
         blokk: blokk
+    });
+});
+
+app.post("/api/register", (req, res) => {
+    const userName = req.body.userName;
+    const password = req.body.password;
+    console.log(userName, password);
+    res.json({
+        status: "ok"
     });
 });
 
